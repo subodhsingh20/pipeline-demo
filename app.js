3d22596b2184a34849b974e95088c024b9719e87
subodh singh <147427359+subodhsingh20@users.noreply.github.com>
2026-02-22 18:00:38 +0530

    Update response message in app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Jenkins CI/CD Pipeline is Working!");
+  res.send("Jenkins CI/CD Pipeline is Working!, great job subodh");
 });
 
 app.listen(3000);
