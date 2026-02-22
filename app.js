cfbf72d1accba46d355a32a792fc6060fb07daf3
subodh singh <subodh_20@outlook.com>
2026-02-22 18:38:01 +0530

    auto deploy works

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Jenkins CI/CD Pipeline is Working!, great job subodh");
+  res.send("Jenkins CI/CD Pipeline is Working!, great job subodh, hehe");
 });
 
 app.listen(3000);
